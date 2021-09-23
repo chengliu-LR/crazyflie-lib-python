2f60eaf792afb03a6626b1b1ce21820a6055ed5a
Cheng <c.liu-10@tudelft.nl>
2021-09-23 18:47:23 +0200

    log param tutorial

--- examples/ricky_log_param.py
+++ examples/ricky_log_param.py
@@ -33,21 +33,71 @@ import time
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.crazyflie.syncLogger import SyncLogger
 from cflib.utils import uri_helper
 
 # URI to the Crazyflie to connect to
 uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7EC')
 
+logging.basicConfig(level=logging.ERROR)
 
-def simple_connect():
-    print('Yeah, I am connected!')
-    time.sleep(3)
-    print('Now I will disconnect!')
+
+# sync log
+def simple_log_sync(scf, logconf):
+    with SyncLogger(scf, log_stab) as logger:
+        for log_entry in logger:
+            timestamp = log_entry[0]
+            data = log_entry[1]
+            logconf_name = log_entry[2]
+            print('[{}][{}]: {}'.format(timestamp, logconf_name, data))
+
+
+# async log working with call_back
+def simple_log_async(scf, logconf):
+    cf = scf.cf
+    cf.log.add_config(logconf)
+    logconf.data_received_cb.add_callback(log_stab_callback)
+    logconf.start()
+    time.sleep(5)
+    logconf.stop()
+
+
+# async log call back function
+def log_stab_callback(timestamp, data, logconf):
+    print('[{}][{}]: {}'.format(timestamp, logconf.name, data))
+
+
+# async param
+def simple_param_async(scf, groupstr, namestr):
+    cf = scf.cf
+    full_name = groupstr + '.' + namestr
+    cf.param.add_update_callback(group=groupstr, name=namestr, cb=param_stab_est_callback)
+
+    time.sleep(1)
+    cf.param.set_value(full_name, 3)
+    time.sleep(1)
+    cf.param.set_value(full_name, 1)
+    time.sleep(1)
+
+
+# async param call back
+def param_stab_est_callback(complete_name, value):
+    print('The crazyflie has parameter ' + complete_name + ' set at number: ' + value)
 
 
 if __name__ == '__main__':
     cflib.crtp.init_drivers()
+    log_stab = LogConfig(name='Stabilizer', period_in_ms=200)
+    log_stab.add_variable('stabilizer.roll', 'float')
+    log_stab.add_variable('stabilizer.pitch', 'float')
+    log_stab.add_variable('stabilizer.yaw', 'float')
+
+    group = 'stabilizer'
+    name = 'estimator'
 
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-        simple_connect()
+        #simple_log_sync(scf, log_stab)
+        #simple_log_async(scf, log_stab)
+        simple_param_async(scf, group, name)
